a37d7d15e6b05c83de22fb8e65f5289fe6796778
King-James-II <jocundjames@gmail.com>
2024-05-05 16:37:53 -0400

    first commit

---
+++ mymath/geometry.py
@@ -0,0 +1,12 @@
+from math import pi
+def area_of_rectangle(length, width):
+    """
+    Takes length and width as input and returns the area of a rectangle.
+    """
+    return length * width
+
+def area_of_circle(radius):
+    """
+    Takes length and breadth as input and returns the area of a rectangle.
+    """
+    return pi * (radius ** 2)
