25dd4e9a37347fa7e2a096416bb5e8455c492a68
King-James-II <105425391+King-James-II@users.noreply.github.com>
2024-05-08 01:34:42 -0400

    Update stats.py
    Added comments.

--- mymath/stats.py
+++ mymath/stats.py
@@ -1,18 +1,34 @@
+# stats.py
+
 def mean(numbers):
     """
-    This function returns the mean of the given list of numbers
+    This function calculates the mean (average) of the given list of numbers.
+
+    Parameters:
+    numbers (list): A list of numbers.
+
+    Returns:
+    float: The mean of the given list of numbers.
     """
-    return sum(numbers)/len(numbers)
+    return sum(numbers) / len(numbers)
+
 def median(numbers):
     """
-    This function returns median of the given list of numbers
+    This function calculates the median of the given list of numbers.
+
+    Parameters:
+    numbers (list): A list of numbers.
+
+    Returns:
+    float: The median of the given list of numbers.
     """
     numbers.sort()
-   
+
     if len(numbers) % 2 == 0:
-       median1 = numbers[len(numbers) // 2]
-       median2 = numbers[len(numbers) // 2 - 1]
-       mymedian = (median1 + median2) / 2
+        median1 = numbers[len(numbers) // 2]
+        median2 = numbers[len(numbers) // 2 - 1]
+        mymedian = (median1 + median2) / 2
     else:
-       mymedian = numbers[len(numbers) // 2]
+        mymedian = numbers[len(numbers) // 2]
+    
     return mymedian
