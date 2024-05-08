e75c07f0e5837f8fbac634d7e70b766a3dee08b2
King-James-II <105425391+King-James-II@users.noreply.github.com>
2024-05-08 01:35:34 -0400

    Update basic.py
    added comments.

--- mymath/basic.py
+++ mymath/basic.py
@@ -1,15 +1,36 @@
 def square(number):
     """
-    This function returns the square of a given number
+    This function returns the square of a given number.
+
+    Parameters:
+    number (int or float): The number to be squared.
+
+    Returns:
+    int or float: The square of the input number.
     """
     return number ** 2
+
 def double(number):
     """
-    This function returns twice the value of a given number
+    This function returns twice the value of a given number.
+
+    Parameters:
+    number (int or float): The number to be doubled.
+
+    Returns:
+    int or float: Twice the value of the input number.
     """
     return number * 2
+
 def add(a, b):
     """
-    This function returns the sum of given numbers
+    This function returns the sum of given numbers.
+
+    Parameters:
+    a (int, float, or str): The first number.
+    b (int, float, or str): The second number.
+
+    Returns:
+    int, float, or str: The sum of the input numbers. If both inputs are strings, they are concatenated.
     """
     return a + b
